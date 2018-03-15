b95136458efa92824e878ae348ed225315f1de33
itzShovon <31797718+itzShovon@users.noreply.github.com>
2018-03-15 10:53:30 +0600

    Add files via upload
    Added few bugs and develop few validation term...

--- Form Validation/RegistrationValidation.php
+++ Form Validation/RegistrationValidation.php
@@ -44,22 +44,50 @@
                 echo "<script type='text/javascript'>alert('$error_message');</script>";
             }
             else{
-                if(isset($_POST['user_name']))
-                    $user_name = $_POST['user_name'];
-                if(isset($_POST['user_email']))
-                    $user_email = $_POST['user_email'];
-                if(isset($_POST['user_roll']))
-                    $user_roll = $_POST['user_roll'];
-                if(isset($_POST['user_phone_no']))
-                    $user_phone = $_POST['user_phone'];
+                $XTemp = 1;
+                
+                if(isset($_POST['user_name'])){
+                    if (!preg_match("/^[a-zA-Z ]*$/",$_POST['user_name'])) {
+//                        $Err = "Only letters and white space allowed";
+                        echo '<script type="text/javascript">alert("Only letters and white space allowed");</script>';
+                        $XTemp = 0;
+                    }
+                    else
+                        $user_name = $_POST['user_name'];
+                }
+                if(isset($_POST['user_email'])){
+                    if (!filter_var($_POST["user_email"], FILTER_VALIDATE_EMAIL)) {
+//                        $Err = "Invalid email format";
+                        echo '<script type="text/javascript">alert("Invalid email format");</script>';
+                        $XTemp = 0;
+                    }
+                    else
+                        $user_email = $_POST['user_email'];
+                }
+                if(isset($_POST['user_roll'])){
+                    if (!preg_match("/^[0-9]{8}/",$_POST['user_roll'])) {
+//                        $Err = "Only number allowed lengh 8";
+                        echo '<script type="text/javascript">alert("Only number allowed with length 8 for roll");</script>';
+                        $XTemp = 0;
+                    }
+                    else
+                        $user_roll = $_POST['user_roll'];
+                }
+                if(isset($_POST['user_phone'])){
+                    if (!preg_match("/^[0-9]{11}/",$_POST['user_phone'])) {
+//                        $Err = "Only number allowed";
+                        echo '<script type="text/javascript">alert("Only number allowed lenght 11 for phone no");</script>';
+                        $XTemp = 0;
+                    }
+                    else
+                        $user_phone = $_POST['user_phone'];
+                }
                 if(isset($_POST['user_father_name']))
                     $user_father_name = $_POST['user_father_name'];
                 if(isset($_POST['user_birthday']))
                     $user_birthday = $_POST['user_birthday'];
                 if(isset($_POST['user_address']))
                     $user_address = $_POST['user_address'];
-                if(isset($_POST['user_phone']))
-                    $user_phone = $_POST['user_phone'];
                 if(isset($_POST['user_SSC']))
                     $user_SSC = $_POST['user_SSC'];
                 if(isset($_POST['user_HSC']))
@@ -67,48 +95,55 @@
                 if(isset($_POST['user_BSc']))
                     $user_BSc = $_POST['user_BSc'];
                 
-                $statement = $db->prepare("INSERT INTO user_info 
-				(
-					user_roll,
-					user_father_name,
-                    user_birthday,
-                    user_address,
-					user_name, 
-					user_email,
-                    user_phone
-				) 
-				VALUES (?,?,?,?,?,?,?)");
-				$statement->execute(array(		
-					$user_roll,
-					$user_father_name,
-                    $user_birthday,
-                    $user_address,
-					$user_name,
-					$user_email,
-                    $user_phone
-				));
-                $statement = $db->prepare("INSERT INTO user_qualification 
-				(
-					user_roll,
-                    user_SSC,
-                    user_HSC,
-                    user_BSc
-				) 
-				VALUES (?,?,?,?)");
-				$statement->execute(array(		
-					$user_roll,
-                    $user_SSC,
-                    $user_HSC,
-                    $user_BSc
-				));
-                
-				$message = "Thanks! information is Saved successfully.";
-				echo "<script type='text/javascript'>alert('$message');</script>";
-				//throw new Exception("Thanks! information is Saved.!");
-                
-                header("Location: Default.php");
-                echo "<script type='text/javascript'>(Math.random();)</script>";
-                exit;
+                if($XTemp == 1){
+                    $statement = $db->prepare("INSERT INTO user_info 
+                    (
+                        user_roll,
+                        user_father_name,
+                        user_birthday,
+                        user_address,
+                        user_name, 
+                        user_email,
+                        user_phone
+                    ) 
+                    VALUES (?,?,?,?,?,?,?)");
+                    $statement->execute(array(		
+                        $user_roll,
+                        $user_father_name,
+                        $user_birthday,
+                        $user_address,
+                        $user_name,
+                        $user_email,
+                        $user_phone
+                    ));
+                    $statement = $db->prepare("INSERT INTO user_qualification 
+                    (
+                        user_roll,
+                        user_SSC,
+                        user_HSC,
+                        user_BSc
+                    ) 
+                    VALUES (?,?,?,?)");
+                    $statement->execute(array(		
+                        $user_roll,
+                        $user_SSC,
+                        $user_HSC,
+                        $user_BSc
+                    ));
+                    
+                    $message = "Thanks! information is Saved successfully.";
+                    echo "<script type='text/javascript'>alert('$message');</script>";
+                    echo "<script type='text/javascript'>alert('Math.random() * 10');</script>";
+                    
+//                    header("Location: Default.php");
+//                    echo "<script type='text/javascript'>(Math.random();)</script>";
+//                    exit;
+                    
+                    
+                    echo "<script type='text/javascript'>alert(rand(10,100));</script>";
+                    //throw new Exception("Thanks! information is Saved.!");
+                }
+                else;
             }
         }
     }catch(Exception $e) {
